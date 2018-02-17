608dab10320f382a481b6086cf5dcb27f80caf4f
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-02-17 16:48:53 +0200

    Added a support for moving the paddles in the CourtScene.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    /// <summary>The velocity of the paddle movement.</summary>
+    public float velocity = 30f;
+
+    /// <summary>The name of the axis controlling this script.</summary>
+    public string axisName = "Left Player";
+
+    /// <summary>A reference to rigid body.</summary>
+    private Rigidbody2D rigidBody;
+    
+    void Awake() {
+        // cache the rigidbody reference to be used in updates.
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        // update rigidbody velocity based on the axis value.
+        var axis = Input.GetAxisRaw(axisName);
+        rigidBody.velocity = new Vector2(0, axis) * velocity;
+    }
+
+}
