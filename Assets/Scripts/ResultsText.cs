4a2681e1334e912b16df48e1e34848ecf622684d
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-02-18 17:50:11 +0200

    Added the overall results into the EndGameScene.

---
+++ Assets/Scripts/ResultsText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ResultsText : MonoBehaviour {
+
+    void Awake() {
+        // get a reference to game context and text mesh.
+        var ctx = GameContext.Instance;
+        var textMesh = GetComponent<TextMesh>();
+
+        // specify the end results into the text component.
+        textMesh.text = ctx.Player2Score + " - " + ctx.Player1Score;
+    }
+
+}
