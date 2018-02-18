1b4709e2edad9ed6413af01ee0ac2ca322ff1376
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-02-18 18:21:27 +0200

    Removed some unnecessary using directives from the scripts.

--- Assets/Scripts/GameContext.cs
+++ Assets/Scripts/GameContext.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// A game context object to hold a cross-scene game definitions.
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 /// <summary>
 /// <para>
